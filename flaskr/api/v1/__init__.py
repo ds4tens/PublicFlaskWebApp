4e0fa5e2af6d7ac989cd54fc0ceb0e0952a8b385
ds4tens <56487555+ds4tens@users.noreply.github.com>
2021-08-30 21:43:38 +0300

    Delete flaskr directory

--- flaskr/api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from flaskr.api.v1.resource.method import SeatingChart
-from flaskr.api import api
-
-
-def api_get():
-    api.add_resource(SeatingChart, '/')
--- flaskr/config/__init__.py
+++
@@ -1,5 +0,0 @@
-from flaskr.parsetools import translator
-
-DATABASE_CONF = translator('flaskr/config_files/database_conf.json')
-
-DATABASE_PROCEDURES = list(translator('flaskr/config_files/db_procedures.json').values())
